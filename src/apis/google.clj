f884884dd90200fd48836a4f61267f7916f23fa6
dom <dominiqn@gmail.com>
2017-02-14 10:01:44 +0100

    activities-search ok need to write online tests

--- src/apis/google.clj
+++ src/apis/google.clj
@@ -7,36 +7,33 @@
             [apis.helpers :refer :all]))
 
 
-(def api-key "AIzaSyCKGpCxvPvgbeLhvJX12P56jooqL9YH8tQ")
-
-(defn extract-items [response]
-  (-> response :body (json/parse-string true) :items))
-
-(defn get-next-page-token [response]
-  (-> response :body (json/parse-string true) :nextPageToken))
-
-(defn http-activities [query-params]
-  (http/get "https://www.googleapis.com/plus/v1/activities"
-                           {:insecure? true
-                            :query-params query-params}))
-
-(defn http-iterate [http size query-params]
-  (->> (http query-params)
-       (iterate #(http (assoc query-params :pageToken (get-next-page-token %))))
-       (take size)
-       (map extract-items) flatten1L))
-
-(defn get-activities [size query-params]
-  (let [request #(http/get "https://www.googleapis.com/plus/v1/activities"
-                           {:insecure? true
-                            :query-params %})]
-    (->> (request query-params)
-         (iterate #(request (assoc query-params :pageToken (get-next-page-token %))))
-         (take size)
-         (map extract-items) flatten1L)))
-
-(def query-params {:key api-key
-                   :query ["#opendata" "#datajournalism"]
+(def *query-params {:key "api-key"
+                   :query ["#term0" "#term1"]
                    :orderBy "recent"
                    :maxResults 20})
+
+(defn http-get [actions query-params]
+  (let [action (clojure.string/join "/" actions)] 
+    (http/get (str "https://www.googleapis.com/plus/v1/" action)
+            {:insecure? true
+             :query-params (merge *query-params query-params)})))
+
+(def activities-search (partial http-get ["activities"]))
+
+(defn http-iterate [http size query-params]
+  (->> [nil  (http query-params)]
+       (iterate (fn [[_ response]]
+                  (if (= 200 (:status response))
+                    (let [body (-> response :body (json/parse-string true))]
+                      [(:items body) (http (assoc query-params :pageToken (:nextPageToken body)))]
+                      ))))
+       next (map first)
+       (take-while identity) (take size)
+       flatten1L))
+
+(defn activities-search [size & query-params]
+  (let [http (partial http-get ["activities"])
+        *query-params (apply hash-map query-params)]
+    (http-iterate http size *query-params)))
+
 
--- test/apis/google_test.clj
+++ test/apis/google_test.clj
@@ -2,24 +2,24 @@
   (:require [midje.sweet :refer :all]
             [apis.google :refer :all]
             [cheshire.core :as json]
+            [net.cgrand.enlive-html :as html]
+            [clj-http.client :as http]
             ))
 
 
-(let [path "dev-resources/google/"
+(let [path "dev-resources/google/" 
+      *query-params [:key "api-key" :query ["data" "business"] :orderBy "recent" :maxResults 20]
       response (json/parse-string (slurp (str path "activities_response.json")) true)
       items (json/parse-string (slurp (str path "items.json")) true)
       [item0 item1 & _] items]
 
 
-  (facts "About `extract-items"
-         (extract-items response) => #(= 10 (count %))
-         (extract-items response) => #(-> % first :id)
+  (facts "About 'activities-search"
+         ((partial http-get 0) 1) => response
+         (activities-search 2 :query "#lol" :maxResults 2) => #(= 20 (count %))
+         (activities-search 2 :query "#lol" :maxResults 2) => #(-> % first :id)
+         (background
+           (http-get & anything) => response)
          )
-
-  (facts "About `http-iterate"
-         (http-iterate http nil nil) => 
-         (provided (http & anything) => {:status "400"}))
-
-
 
   )
